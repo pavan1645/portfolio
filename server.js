6b6e00b8128689cd748ab01990a3d16f96a6f781
pavan <pavan1645@gmail.com>
2019-07-09 21:15:28 +0530

    changed port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const PORT = 8000;
+const PORT = 8003;
 
 app.use(express.static(__dirname + '/dist/'));
 app.listen(PORT, () => console.log("Listening on PORT", PORT));
